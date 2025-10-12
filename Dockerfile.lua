c82a0af12f88f4a11768749d011721ffc98f84b0
widhaprasa <widhaprasa@gmail.com>
2025-10-12 17:01:33 +0700

    Update golang and nginx for lua

--- Dockerfile.lua
+++ Dockerfile.lua
@@ -1,5 +1,5 @@
 # Step 1: Build the Go app
-FROM golang:1.25.1-alpine3.22 as builder
+FROM golang:1.25.2-alpine3.22 as builder
 
 WORKDIR /app
 COPY . .
@@ -8,7 +8,7 @@ COPY . .
 RUN go build -o app .
 
 # Step 2: Create the image with Nginx and the Go binary
-FROM fabiocicerchia/nginx-lua:1.28.0-alpine3.22.1
+FROM fabiocicerchia/nginx-lua:1.28.0-alpine3.22.2
 
 # Install supervisor to run multiple processes
 RUN apk add --no-cache supervisor
